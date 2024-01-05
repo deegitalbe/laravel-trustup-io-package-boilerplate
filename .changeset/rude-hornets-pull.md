6d40bbbef3c370ea39b8801759ce0efdd1668aa6
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2024-01-05 15:45:57 +0100

    :sparkles: Stable version.

---
+++ .changeset/rude-hornets-pull.md
@@ -0,0 +1,5 @@
+---
+"@deegital/laravel-trustup-io-package-boilerplate": major
+---
+
+Stable version with changeset.
--- stubs/scripts/bootstrap.sh
+++ stubs/scripts/bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+docker run \
+  -it \
+  --rm \
+  --user node:node \
+  -v "$PWD":/usr/src/app \
+  -w /usr/src/app node:18-alpine \
+  yarn install && \
 docker run --rm \
     -u "$(id -u):$(id -g)" \
     -v "$(pwd):/var/www/html" \
